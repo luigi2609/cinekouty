d7c588ecd142bb3246af08631e5556131d4673cd
luigi2609 <luigi.cinelli2609@gmail.com>
2018-07-13 17:43:18 +0200

    Preparing stuff for a small chat

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.use(express.static('client'));
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+    res.send('Hello world');
 });
 
-app.get('/pippo', function (req, res) {
-    res.send('Pluto');
-});
+
+// 1° creare get su /api/chat e rispondere contenut messages
+const messages = ['Ciao', 'Come va?', 'Tutto bene grazie!'];
+// Usare res.json(messages);
+
+
+// 2° create post su /api/chat e aggiungere il messaggio a messages array
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
