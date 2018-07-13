0c468d3940f2cf864ed5bd7e42ef34af5e3badc8
luigi2609 <luigi.cinelli2609@gmail.com>
2018-07-13 16:48:46 +0200

    Server/client

---
+++ server/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/pippo', function (req, res) {
+    res.send('Pluto');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
