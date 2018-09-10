d1751dd2ff104108d59100e394a2b697a4945c57
Andrea Koutifaris <kouty79@gmail.com>
2018-09-10 14:24:10 +0200

    showing messages on html

--- server/index.js
+++ server/index.js
@@ -40,6 +40,6 @@ app.post('/message', (req, res) => {
 
 
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(3291, function () {
+    console.log('Example app listening on port 3291!');
 });
